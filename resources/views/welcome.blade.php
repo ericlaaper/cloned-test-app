32c53823a2a115b8330710bb2ad42f221f04075c
Eric Laaper <e.laaper@continuitas.nl>
2022-04-02 22:42:50 +0200

    onw files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Original fresh</title>
+        <title>Original cloned</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -82,7 +82,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Een nog iets eerste aanpassingen. en nog een
+                                    Een nog iets eerste aanpassingen.
                                 </div>
                             </div>
                         </div>
